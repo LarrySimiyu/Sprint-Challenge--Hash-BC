2a4ce03ce9ff22177b5204dd37ee2ca26a15c607
LarrySimiyu <larry.simiyu@gmail.com>
2019-12-06 09:59:23 -0800

    mvp

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -1,15 +1,9 @@
 import hashlib
 import requests
-
 import sys
-
 from uuid import uuid4
-
 from timeit import default_timer as timer
-
 import random
-
-
 def proof_of_work(last_proof):
     """
     Multi-Ouroboros of Work Algorithm
@@ -18,30 +12,32 @@ def proof_of_work(last_proof):
     - IE:  last_hash: ...AE9123456, new hash 123456888...
     - p is the previous proof, and p' is the new proof
     - Use the same method to generate SHA-256 hashes as the examples in class
+    - Note:  We are adding the hash of the last proof to a number/nonce for the new proof
     """
-
     start = timer()
 
-    print("Searching for next proof")
+    print("Looking for next proof")
     proof = 0
+    proof = 1000000 
     #  TODO: Your code here
+    last_hash = hashlib.sha256(f'{last_proof}'.encode()).hexdigest()
+    while not valid_proof(last_hash, proof):
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
-
-
 def valid_proof(last_hash, proof):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
-    the hash of the last proof match the first six characters of the hash
-    of the new proof?
-
-    IE:  last_hash: ...AE9123456, new hash 123456E88...
+    the hash of the last proof match the first six characters of the proof?
+    IE:  last_hash: ...AE9123456, new hash 123456888...
     """
 
-    # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
 
+    # 6 zeros
+    return guess_hash[:6] == last_hash[-6:]
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -49,17 +45,14 @@ if __name__ == '__main__':
         node = sys.argv[1]
     else:
         node = "https://lambda-coin.herokuapp.com/api"
-
     coins_mined = 0
-
     # Load or create ID
     f = open("my_id.txt", "r")
     id = f.read()
     print("ID is", id)
     f.close()
-
     if id == 'NONAME\n':
-        print("ERROR: You must change your name in `my_id.txt`!")
+        print("ERROR: Change your name in `my_id.txt`!")
         exit()
     # Run forever until interrupted
     while True:
@@ -67,10 +60,9 @@ if __name__ == '__main__':
         r = requests.get(url=node + "/last_proof")
         data = r.json()
         new_proof = proof_of_work(data.get('proof'))
-
         post_data = {"proof": new_proof,
-                     "id": id}
-
+                     "id": id
+                     }
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
         if data.get('message') == 'New Block Forged':
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,20 +4,25 @@ from hashtables import (HashTable,
                         hash_table_remove,
                         hash_table_retrieve,
                         hash_table_resize)
-
-
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
-
-
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
+    #Hash each ticket
+    for ticket in tickets:
+        print(ticket.source, ticket.destination)
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    """
-    YOUR CODE HERE
-    """
+    
+    # Find location
+    locat = hash_table_retrieve(hashtable, "NONE")
 
-    return route
+    # Retrieve value using location
+    for i in range(0, length):
+        route[i] = locat
+        locat = hash_table_retrieve(hashtable, locat)
+
+    return route 
